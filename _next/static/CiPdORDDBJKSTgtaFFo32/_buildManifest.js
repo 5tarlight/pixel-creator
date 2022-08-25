6d0b58aee1bcbb01e1db227e07b238e89a9d9c0a
YEAHx4 <developerstarlight@gmail.com>
2022-08-25 14:10:51 +0900

    Deploy Next.js to gh-pages

---
+++ _next/static/CiPdORDDBJKSTgtaFFo32/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/146-34b0a09bcdb589b0.js","static/chunks/pages/index-1f2ee46920808a58.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
